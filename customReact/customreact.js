a53fbefacd69b682155c8cec750c1a0849b9ff4e
Anish Sharma <anish.m.sharma2004@gmail.com>
2026-01-25 09:13:52 +0300

    just me moving to office pc

---
+++ customReact/customreact.js
@@ -0,0 +1,34 @@
+function customRender(reactElement, container) {
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.prop.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    // we have made this loop to avoid typing the same line again and again as shown in the first example
+
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank'
+    },
+    children: 'Click me to go to Google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
